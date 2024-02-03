13d9206c49d5a1a6053bf6d491bfeef202ccdf5d
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-02-03 00:24:24 -0300

    feat :sparkles: adicionando  player na tela

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 import pygame
 
-#Inicializando o pyGame
+# Inicializando o pygame
 pygame.init()
 
-#Criando a Janela
+# Criando a janela
 screen = pygame.display.set_mode((800, 600))
 
-#Loop do Jogo
+# Adicionando título
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("img/ufo.png")
+pygame.display.set_icon(icon)
+
+#Adicionando Nave
+playerImg = pygame.image.load("img/player.png")
+playerX = 370
+playerY = 480
+
+#função que cria a nave
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+# Loop do jogo
 running = True
 while running:
+    
+    #Cor da tela
+    screen.fill((0, 0, 0))
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # Preenche a tela com a cor vermelha
+
+    #chamando player na tela
+    player()
+    # Atualiza a tela
+    pygame.display.update()
